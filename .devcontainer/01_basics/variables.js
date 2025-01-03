8a754a28e5ad8fe23d60ad13e746c617824f0db9
Saikat Samanta <116175236+Saikat-93@users.noreply.github.com>
2025-01-03 15:04:18 +0000

    first

---
+++ .devcontainer/01_basics/variables.js
@@ -0,0 +1,10 @@
+const roll=29
+let account="saikatsamanta624@gm.com"
+var accountpass ="1234"
+accity="kolkata"
+//roll=30 //not allowed
+account="saikat.89"
+accountpass="234"
+accity="panskura"
+console.log(roll);
+console.table([roll,account,accountpass,accity])
